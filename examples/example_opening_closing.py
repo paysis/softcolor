aeedcd82658636f7db41efa4cf0c475d3fe59add
Pedro Bibiloni <pedro@bibiloni.es>
2018-12-22 21:17:25 +0100

    Improve style

--- examples/example_opening_closing.py
+++ examples/example_opening_closing.py
@@ -1,4 +1,3 @@
-
 import matplotlib.pyplot as plt
 from skimage import io
 from skimage.morphology import disk
@@ -17,12 +16,10 @@ if __name__ == "__main__":
         img_closing = morphology.closing(img, structuring_element=se)
     print('Time elapsed: {} s'.format(t.interval))
 
-    fig, axs = plt.subplots(nrows=2, ncols=2)
+    _, axs = plt.subplots(nrows=2, ncols=2)
     [a.axis('off') for a in axs.flat]
-
     axs[0, 1].imshow(se)
     axs[0, 0].imshow(img)
     axs[1, 0].imshow(img_opening)
     axs[1, 1].imshow(img_closing)
-
     plt.show()
