ccbee20b7fea9810696789c59657b53787a26c1f
Pedro Bibiloni <pedro@bibiloni.es>
2018-12-22 20:57:50 +0100

    Fix axis to 2

--- softcolor/distance_between_images.py
+++ softcolor/distance_between_images.py
@@ -2,12 +2,12 @@ import numpy as np
 
 
 def euclidean_distance(x, y):
-    return np.linalg.norm(x-y, axis=3)
+    return np.linalg.norm(x-y, axis=2)
 
 
-def l_1_distance(x, y):
-    return np.linalg.norm(x-y, ord=1, axis=3)
+def l_one_distance(x, y):
+    return np.linalg.norm(x-y, ord=1, axis=2)
 
 
 def l_inf_distance(x, y):
-    return np.linalg.norm(x-y, ord=np.inf, axis=3)
+    return np.linalg.norm(x-y, ord=np.inf, axis=2)
