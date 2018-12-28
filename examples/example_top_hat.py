6edfee4332c700d5f23c19348639f504c66b5ef1
Pedro Bibiloni <pedro@bibiloni.es>
2018-12-28 15:57:51 +0100

    Example of Color Top-Hat

---
+++ examples/example_top_hat.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from skimage import io
+from skimage.morphology import disk
+
+from examples.utils import Timer
+from softcolor.morphology import MorphologyInCIELab, soften_structuring_element
+
+if __name__ == "__main__":
+    img = io.imread('images/mandril_color.tif')
+
+    morphology = MorphologyInCIELab()
+    se = disk(5)
+    se = soften_structuring_element(se)
+    with Timer() as t:
+        img_tophat_opening = morphology.tophat_opening(img, structuring_element=se)
+        img_tophat_closing = morphology.tophat_closing(img, structuring_element=se)
+    print('Time elapsed: {} s'.format(t.interval))
+
+    _, axs = plt.subplots(nrows=2, ncols=2)
+    [a.axis('off') for a in axs.flat]
+    axs[0, 1].imshow(se)
+    axs[0, 0].imshow(img)
+    axs[1, 0].imshow(img_tophat_opening)
+    axs[1, 1].imshow(img_tophat_closing)
+    plt.show()
